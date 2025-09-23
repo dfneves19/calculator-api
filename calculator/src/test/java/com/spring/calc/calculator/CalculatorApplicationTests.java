1d8921773e0c7eeb684e67a5fc9ea15c94db11ca
Diogo Neves <diogo.f.neves.2002@gmail.com>
2025-09-23 13:24:04 +0100

    first commit

---
+++ calculator/src/test/java/com/spring/calc/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.calc.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
