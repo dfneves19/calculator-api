056be970656112354e1b7e88fbac986f05e47521
Diogo Neves <diogo.f.neves.2002@gmail.com>
2025-09-23 16:54:57 +0100

    implement calculator service

---
+++ calculator/src/main/java/com/spring/calc/calculator/service/CalculatorService.java
@@ -0,0 +1,31 @@
+package com.spring.calc.calculator.service;
+
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+@Service
+public class CalculatorService {
+
+    public BigDecimal sum(BigDecimal a, BigDecimal b) {
+        return a.add(b);
+    }
+
+    public BigDecimal subtract(BigDecimal a, BigDecimal b) {
+        return a.subtract(b);
+    }
+
+    public BigDecimal multiply(BigDecimal a, BigDecimal b) {
+        return a.multiply(b);
+    }
+
+    public BigDecimal divide(BigDecimal a, BigDecimal b) {
+        if (b.compareTo(BigDecimal.ZERO) == 0){
+            throw new ArithmeticException("Divide by zero");
+        }else{
+            return a.divide(b,10, RoundingMode.HALF_UP);
+        }
+    }
+}
+
